dddbd8a8d6ed9096627291be24b4eeb22a88afe2
upeshchowdary <upeshchowdary.k@gmail.com>
2025-10-12 20:53:07 +0530

    Fix deployment config or latest changes

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Expense-tracker/',
   plugins: [react()],
   // vite.config.js
 
-  base: '/Expense-tracker/', // <-- must be your actual repo name!
+   // <-- must be your actual repo name!
 
 })
